e8ec70f96a1fba8ce8fae4a4187083bb5fa2bc80
Janhavi Kale <81229872+janhavikale06@users.noreply.github.com>
2023-09-14 14:36:51 +0530

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Scaffold(
+      backgroundColor: Colors.white,
+    );
+
+
+  }
+
+}
---
+++ sign_up.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:mental_health/home_page.dart';
+
+// ignore: camel_case_types
+class signUp extends StatelessWidget {
+  const signUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/blob1.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 25, top: 105),
+              child: const Text(
+                'Create Account',
+                style: TextStyle(fontSize: 43, color: Color(0xFFAA77FF), fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.47,
+                right: 35,
+                left: 35,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: const Color(0xFFD8B4F8),
+                        filled: true,
+                        hintText: 'Username',
+                        hintStyle: const TextStyle(color: Color(0xFFAA77FF)),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Color(0xFFAA77FF)),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        fillColor: const Color(0xFFD8B4F8),
+                        filled: true,
+                        hintText: 'Email',
+                        hintStyle: const TextStyle(color: Color(0xFFAA77FF)),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Color(0xFFAA77FF)),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        fillColor: const Color(0xFFD8B4F8),
+                        filled: true,
+                        hintText: 'Password',
+                        hintStyle: const TextStyle(color: Color(0xFFAA77FF)),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Color(0xFFAA77FF)),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 65,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const homePage()),
+                        );
+                        // Handle sign up button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFAA77FF),
+                        minimumSize: const Size(200, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(fontSize: 23),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
