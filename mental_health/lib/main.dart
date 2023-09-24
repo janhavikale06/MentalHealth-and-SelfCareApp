cdbde76eb08f6900d7229b9f2d1259574b3ffe46
janhavikale06 <janhavikale06@gmail.com>
2023-09-24 14:19:45 +0530

    Mental Health And Self Care App

--- mental_health/lib/main.dart
+++ mental_health/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health/splash.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +13,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter UI',
       theme: ThemeData(
-        
       ),
       home: const Splash(),
     );
